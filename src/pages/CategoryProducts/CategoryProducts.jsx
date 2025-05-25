8b4fe8a0ef502ea1c3bfa8b4a9c307e9c0dfd92b
Hager Amr <hageramr320@gmail.com>
2025-05-25 23:01:01 +0300

    start with category products

--- src/pages/CategoryProducts/CategoryProducts.jsx
+++ src/pages/CategoryProducts/CategoryProducts.jsx
@@ -5,17 +5,60 @@ import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { toast } from "react-toastify";
 
 const CategoryProducts = () => {
-  const { category } = useParams();
-  const { productsCategory, isError, error, isLoading } = useProductsContext();
+ const { category } = useParams();
+  const { getProductCategry, setPage, page, limit } = useProductsContext();
+  
+  const { 
+    data: productsCategory, 
+    isError, 
+    error, 
+    isLoading 
+  } = getProductCategry(category, page, limit);
   
   if (isLoading) return <LoadingSpinner />;
-  if (isError) return toast.error(`error : ${error.message}`)
+  if (isError) {
+    toast.error(`Error: ${error.message}`);
+    return null;
+  }
+
   return (
-    <>
-      {productsCategory?.map((product) => (
-        <div key={product._id}>{product.title}</div>
-      ))}
-    </>
+    <div>
+      <Box sx={{ paddingLeft: { md: "290px" } }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            gap: 4,
+            justifyContent: "center",
+            marginY: 4,
+          }}
+        >
+          {productsCategory?.data?.map((product) => (
+            <ProductCard
+              key={product._id}
+              product={{
+                thumbnail: product.thumbnail,
+                title: product.title,
+                avgRating: product.avgRating,
+                price: product.price,
+                label: product.label || "no label",
+                _id: product._id,
+              }}
+              onAddToCart={() =>console.log("dd")}
+              onToggleFavorite={() => console.log("dd")}
+              onProductClick={() => console.log("dd")}
+              sx={{ width: "290px", aspectRatio: "2/3", height: "66%" }}
+            />
+          ))}
+        </Box>
+        <PaginationComponent
+          currentPage={page}
+          handlePagination={setPage}
+          totalPages={productsCategory?.totalPages || 1}
+        />
+      </Box>
+    </div>
   );
 };
 
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import { Typography, Box, IconButton, Button } from "@mui/material";
 import Review from "../../components/Review/Review";
 import RelatedProducts from "../../components/RelatedProducts/RelatedProducts";
-import { Navigate, useParams } from "react-router";
+import { Navigate, useNavigate, useParams } from "react-router";
 import { useProductsContext } from "../../context/ProductsContext";
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 import { toast } from "react-toastify";
@@ -43,7 +43,6 @@ export default function ProductDetails() {
 
   const prd = product.data[0];
 
-
   const handleFavoriteClick = (e) => {
     e.stopPropagation();
     if (isFavorited(id)) {
@@ -52,170 +51,179 @@ export default function ProductDetails() {
     } else {
       addToFav(id);
       setFavorited(true);
-    }
-  // check user is logged ?
-  const user = localStorage.getItem("user");
+    }}
+    // check user is logged ?
+    const user = localStorage.getItem("user");
 
-  // add to cart
-  const handleAddToCart = () => {
-    if (!user) {
-      toast.error("Please log in to add items to cart");
-      return;
-    }
-    toast.success("item added to cart successfully");
-    addToCart(prd._id);
-  };
+    // add to cart
+    const handleAddToCart = () => {
+      if (!user) {
+        toast.error("Please log in to add items to cart");
+        return;
+      }
+      toast.success("item added to cart successfully");
+      addToCart(prd._id);
+    };
 
-  console.log(product);
-  return (
-    <Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          p: { xs: 2, md: 6 },
-          overflowX: "hidden",
-        }}>
-        {/* Image Section */}
+    console.log(product);
+    return (
+      <Box>
         <Box
           sx={{
             display: "flex",
-            flex: 1,
-            width: "100%",
-            height: { xs: "60vh", sm: "430px", md: "75vh", lg: "75vh" },
-          }}>
-          <img
-            src={prd.thumbnail}
-            alt="coffee shop"
-            style={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              borderRadius: "1.5rem",
-            }}
-          />
-        </Box>
-
-        {/* Text Section */}
-        <Box
-          sx={{
-            display: "flex",
-            flex: 1,
-            flexDirection: "column",
-            width: "100%",
-            height: "100%",
-            px: { xs: 2, md: 6 },
-            py: { xs: 2, md: 4 },
-            alignItems: { xs: "center", md: "flex-start" },
-            textAlign: { xs: "center", md: "left" },
-          }}>
+            flexDirection: { xs: "column", md: "row" },
+            p: { xs: 2, md: 6 },
+            overflowX: "hidden",
+          }}
+        >
+          {/* Image Section */}
           <Box
             sx={{
               display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
+              flex: 1,
               width: "100%",
-            }}>
-            <Typography
-              variant="h3"
-              sx={{
-                mb: 2,
-                fontSize: { xs: "1.75rem", sm: "2rem", md: "2.5rem" },
-                color: "var(--primary)",
-                fontFamily: "Pacifico, cursive",
-              }}>
-              {prd.title}
-            </Typography>
-
-            {/* Add to Fav */}
-            <IconButton onClick={handleFavoriteClick}>
-              {favorited ? (
-                <FavoriteIcon sx={{ fontSize: "2.5rem", color: red[800] }} />
-              ) : (
-                <FavoriteBorderOutlinedIcon
-                  sx={{ fontSize: "2.5rem", color: "var(--primary)" }}
-                />
-              )}
-            </IconButton>
+              height: { xs: "60vh", sm: "430px", md: "75vh", lg: "75vh" },
+            }}
+          >
+            <img
+              src={prd.thumbnail}
+              alt="coffee shop"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                borderRadius: "1.5rem",
+              }}
+            />
           </Box>
 
+          {/* Text Section */}
           <Box
             sx={{
-              background: "var(--custom-gradient)",
-              // bgcolor: "var(--gold)",
-              height: "5px",
-              width: { xs: "70%", md: "18rem" },
-              alignSelf: "flex-start",
-              mb: 4,
+              display: "flex",
+              flex: 1,
+              flexDirection: "column",
+              width: "100%",
+              height: "100%",
+              px: { xs: 2, md: 6 },
+              py: { xs: 2, md: 4 },
+              alignItems: { xs: "center", md: "flex-start" },
+              textAlign: { xs: "center", md: "left" },
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Typography
+                variant="h3"
+                sx={{
+                  mb: 2,
+                  fontSize: { xs: "1.75rem", sm: "2rem", md: "2.5rem" },
+                  color: "var(--primary)",
+                  fontFamily: "Pacifico, cursive",
+                }}
+              >
+                {prd.title}
+              </Typography>
+
+              {/* Add to Fav */}
+              <IconButton onClick={handleFavoriteClick}>
+                {favorited ? (
+                  <FavoriteIcon sx={{ fontSize: "2.5rem", color: red[800] }} />
+                ) : (
+                  <FavoriteBorderOutlinedIcon
+                    sx={{ fontSize: "2.5rem", color: "var(--primary)" }}
+                  />
+                )}
+              </IconButton>
+            </Box>
+
+            <Box
+              sx={{
+                background: "var(--custom-gradient)",
+                // bgcolor: "var(--gold)",
+                height: "5px",
+                width: { xs: "70%", md: "18rem" },
+                alignSelf: "flex-start",
+                mb: 4,
+              }}
+            />
+
+            <Typography
+              variant="h5"
+              sx={{
+                mb: 2,
+                color: "var(--tertiary)",
+              }}
+            >
+              Ingredients
+            </Typography>
+
+            <Typography
+              variant="h5"
+              sx={{
+                mb: 4,
+                color: "var(--main-text)",
+              }}
+            >
+              {prd.description}
+            </Typography>
+
+            <Typography
+              variant="h6"
+              sx={{
+                mb: 6,
+                fontWeight: "700",
+                color: "var(--primary)",
+                fontFamily: "Playpen Sans Hebrew",
+              }}
+            >
+              Price: {prd.price}
+            </Typography>
+
+            <Button
+              onClick={handleAddToCart}
+              sx={{
+                width: { xs: "80%", sm: "80%" },
+                p: 2,
+                borderRadius: "1rem",
+                bgcolor: "var(--primary)",
+                color: "var(--main-background)",
+                fontWeight: "bold",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                cursor: "pointer",
+                gap: 2,
+                mx: "auto",
+                textAlign: "center",
+                transition: "background-color 0.3s ease",
+                "&:hover": {
+                  bgcolor: "var(--main-text)",
+                },
+              }}
+            >
+              <ShoppingCartIcon />
+              Add To Cart
+            </Button>
+          </Box>
+        </Box>
+        <Review></Review>
+        <Box sx={{ maxWidth: "1200px", mx: "auto", px: { xs: 2, md: 6 } }}>
+          <RelatedProducts
+            categoryId={prd.categoryID}
+            currentProductId={prd._id}
+            onProductClick={(prd) => {
+              navigate(`/menu-items/${prd}`);
             }}
           />
-
-          <Typography
-            variant="h5"
-            sx={{
-              mb: 2,
-              color: "var(--tertiary)",
-            }}>
-            Ingredients
-          </Typography>
-
-          <Typography
-            variant="h5"
-            sx={{
-              mb: 4,
-              color: "var(--main-text)",
-            }}>
-            {prd.description}
-          </Typography>
-
-          <Typography
-            variant="h6"
-            sx={{
-              mb: 6,
-              fontWeight: "700",
-              color: "var(--primary)",
-              fontFamily: "Playpen Sans Hebrew",
-            }}>
-            Price: {prd.price}
-          </Typography>
-
-          <Button
-            onClick={handleAddToCart}
-            sx={{
-              width: { xs: "80%", sm: "80%" },
-              p: 2,
-              borderRadius: "1rem",
-              bgcolor: "var(--primary)",
-              color: "var(--main-background)",
-              fontWeight: "bold",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "pointer",
-              gap: 2,
-              mx: "auto",
-              textAlign: "center",
-              transition: "background-color 0.3s ease",
-              "&:hover": {
-                bgcolor: "var(--main-text)",
-              },
-            }}>
-            <ShoppingCartIcon />
-            Add To Cart
-          </Button>
         </Box>
       </Box>
-      <Review></Review>
-      <Box sx={{ maxWidth: "1200px", mx: "auto", px: { xs: 2, md: 6 } }}>
-        <RelatedProducts
-          categoryId={prd.categoryID}
-          currentProductId={prd._id}
-          onProductClick={(prd) => {
-            navigate(`/menu-items/${prd}`);
-          }}
-        />
-      </Box>
-    </Box>
-  );
-}
+    );
+  };

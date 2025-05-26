17fc3a0f30ab5400878728a5b6a68166383bcd76
Eman Radwan <emanalaa11499@gmail.com>
2025-05-26 14:33:32 +0300

    edit styling

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:7500", // access api url from .env file
+  baseURL: import.meta.env.VITE_API_URL, // access api url from .env file
   withCredentials: true, // important for http only cookie auth
 });
 
